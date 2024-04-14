36aa6fd301f44f277133b2cd7c5d966e9f67591e
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2024-04-14 08:42:22 +0530

    Update __main__.py

--- SHIELD3/__main__.py
+++ SHIELD3/__main__.py
@@ -4,7 +4,7 @@ from pyrogram import idle
 from SHIELD3 import SHIELD3
 from SHIELD3.modules import ALL_MODULES
 
-LOGGER_ID = -1002010924139
+LOGGER_ID = -1001961907996
 
 loop = asyncio.get_event_loop()
 
@@ -13,8 +13,8 @@ async def roy_bot():
         importlib.import_module("SHIELD3.modules." + all_module)
     print("♥︎ B𝗈𝗍 Started Successfully.")
     await idle()
-    print("♥︎ Don't edit baby, otherwise you get an error. @H_CC_HELP")
-    await SHIELD3.send_message(LOGGER_ID, "**✦ ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ.\n\n✦ ᴊᴏɪɴ - @The_Friendz**")
+    print("♥︎ Don't edit baby, otherwise you get an error. @Boy_Girl_Dp")
+    await SHIELD3.send_message(LOGGER_ID, "**✦ ɪ ᴀᴍ ᴀʟɪᴠᴇ ʙᴀʙʏ.\n\n✦ ᴊᴏɪɴ - @Freinds_club**")
 
 if __name__ == "__main__":
     loop.run_until_complete(roy_bot())
